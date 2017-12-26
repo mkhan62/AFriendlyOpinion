691f5a544430d3175c12f3d865a45d9edf8dfd7c
mkhan62 <mkhan62@hawk.iit.edu>
2017-12-25 23:12:23 -0600

    changes for deployment

--- AFriendsOpinion/settings.py
+++ AFriendsOpinion/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import bootstrap3
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'friend_app',
-    'bootstrap3'
+
 
 
 ]
